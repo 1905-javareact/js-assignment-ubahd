00c4a87ad146e7a80a3bf2959bed4428ea2d7791
ubahd <ubah.davidt@gmail.com>
2019-05-13 15:39:06 -0400

    Ready

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -3,7 +3,7 @@ Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
 
 //using class
-class person {
+class Person {
     constructor(name, age){
         this.name = name
         this.age = age
@@ -11,14 +11,16 @@ class person {
 
 }
 
-let human = new person('David', 50)
-console.log(human)
-
 //using literals
 let someone = {
-    name = 'David',
+   name = 'David',
     age = 50
 }
+
+let human = new Person('David', 50)
+console.log(human)
+
+
 
 
 
